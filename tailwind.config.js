77b8826dff256de6adf9c3678c36deef5b8aac3e
PaingSC <paingsoechit.job@gmail.com>
2023-07-29 16:05:09 +0630

    Adding project section

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,23 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'main': '#A7A7A7',
-        'sub': '#CCCCCCC',
+        main: "#A7A7A7",
+        sub: "#CCCCCCC",
       },
       fontSize: {
-        'base': ['20px', '1.75'],
-        'sub-ttl': ['32px', '1.75'],
-        'h2-ttl': ['48px', '1.75']
-      }
-      
+        base: ["20px", "1.75"],
+        "sub-ttl": ["32px", "1.75"],
+        "h2-ttl": ["48px", "1.75"],
+        "pj-ttl": ["28px", "1.75"],
+        "pj-txt": ["18px", "1.75"],
+      },
+      // fontWeight: {
+      //   light: ["300"],
+      //   regular: ["400"],
+      //   medium: ["500"],
+      //   bold: ["700"],
+      // },
     },
   },
   plugins: [],
-}
+};
